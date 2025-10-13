cc742ef8a41615007774e4f2801743b37f211805
Peter Christian Quincy Blasser <peterb@imac.speedport.ip>
2025-10-13 09:49:14 +0200

    earth

--- earth_ash.h
+++ earth_ash.h
@@ -9,6 +9,11 @@
 //branchy integrative hysteresis on the earth
 //to switch modes
 
+//sorry this is branch could be xor and shifties in asm
+//if > thresh, int++, if int>32, thresh go low, etc 
+//> >-32 thresh high real softserve code
+
+
 //input out to hear sampling rate
 
 //input sampled triggered by flip or skip
